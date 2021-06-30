9fe999cb89b25c673fa9fa4d0a00e40b22222d04
Francisco Sant'Anna <francisco.santanna@gmail.com>
2021-06-30 20:11:11 -0300

    ...

--- src/client.c
+++ src/client.c
@@ -15,6 +15,10 @@ void on_write (uv_write_t* req, int status);
 void on_read (uv_stream_t* stream, ssize_t nread, const uv_buf_t* buf);
 void on_write_start (uv_write_t* req, int status);
 
+void app (uint64_t now, int id, int data);
+
+static int state = 0;
+
 void main (void) {
     uv_loop_t loop;
     uv_loop_init(&loop);
@@ -28,7 +32,12 @@ void main (void) {
     uv_connect_t conn;
     uv_tcp_connect(&conn, &tcp, (struct sockaddr*)&dest, on_connect);
 
-    uv_run(&loop, UV_RUN_DEFAULT);
+    while (1) {
+        int more = uv_run(&loop, UV_RUN_NOWAIT);
+        if (more && state==1) {
+            app(uv_now(&loop), 0, 0);
+        }
+    }
     return;
 }
 
@@ -48,8 +57,8 @@ void on_read (uv_stream_t* client, ssize_t nread, const uv_buf_t* buf) {
     assert(nread > 0);
     printf("read ok: %ld\n", nread);
 
-    static int state = 0;
     if (state == 0) {
+        state = 1;
         assert(nread == 1);
         assert(buf->base[0] == 0);
         puts("recv START");
@@ -69,3 +78,19 @@ void on_alloc (uv_handle_t* handle, size_t size, uv_buf_t* buf) {
     printf("alloc ok: %ld\n", size);
 	*buf = uv_buf_init(malloc(size), size);
 }
+
+void app (uint64_t now, int id, int data) {
+    printf("now=%ld, id=%d, data=%d\n", now, id, data);
+
+#if 0
+    static int nxt = 0;
+    if (nxt == 0) {
+        nxt = rand() % 10000;
+    } else if (now >= nxt) {
+        nxt = now + rand() % 10000;
+        evt_t evt = { EVT_XXX, rand()%100 };
+        queue_add(evt);
+    }
+#endif
+}
+
