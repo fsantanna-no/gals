c7643f7ad3d2a4a8d439978b0fb6a617960da84a
Francisco Sant'Anna <francisco.santanna@gmail.com>
2021-07-21 20:15:45 -0300

    ...

--- paper/logs/x.lua
+++ paper/logs/x.lua
@@ -2,10 +2,14 @@
 -- $ lua5.4 x.lua rem.xxx > rem.x
 -- $ lua5.4 y.lua rem.x > rem.y
 
-
-function dec (n)
-    return math.floor(n*100)/100
+--[[
+function int (n)
+    return math.floor(n)
 end
+function dec (n)
+    return int(n*100)/100
+end
+]]
 
 local f = assert(io.open(...))
 local l = f:read'*l'
@@ -13,9 +17,8 @@ print('  N-FPS-RATE', 'FRAMES', 'EVT', 'RTT', 'LAT', 'DRIFT', 'FREEZE', 'LATE')
 while l do
     local dir,n,fps,evt,time,frames,evts,rtt,lat,_,drift,_,freeze,_,late = string.match(l, "XXX ; ([^ ]*) ; 50.110. ; 600 ; (%d+) ; (%d+) ; (%d+) ; (%d+) ; (%d+) ; (%d+) ; %s*([^ ]*) ; %s*([^ ]*) ; (%d+);([^ ]*) ; (%d+);([^ ]*) ; (%d+);([^ ]*)")
     --print(l)
-    print(string.format('%03d',n)..'-'..string.format('%03d',fps)..'-'..string.format('%05d',math.floor(evt/n)),
-          dec(frames/(time/(1000/fps))),
-          dec(evts/(time/evt)),
+    print(string.format('%03d',n)..'-'..string.format('%03d',fps)..'-'..string.format('%05d',evt/n),
+          frames/(time/(1000/fps)), evts/(time/evt),
           rtt, lat, drift, freeze, late)
     l = f:read'*l'
 end
--- paper/logs/y.lua
+++ paper/logs/y.lua
@@ -1,39 +1,52 @@
+--[[
+function int (n)
+    return math.floor(n)
+end
+function dec (n)
+    return math.floor(n*100)/100
+end
+]]
+
 local f = assert(io.open(...))
 local l = f:read'*l'
 print(l)
 l = f:read'*l'
+t = {}
 while l do
-    local LBL,FRAMES,EVT,RTT,LAT,DRIFT,FREEZE,LATE =
+    local lbl,frames,evt,rtt,lat,drift,freeze,late =
         string.match(l, "([^%s]*)%s([^%s]*)%s([^%s]*)%s([^%s]*)%s([^%s]*)%s([^%s]*)%s([^%s]*)%s([^%s]*)")
-    local a1,a2,a3,a4,a5,a6,a7 = tonumber(FRAMES),tonumber(EVT),tonumber(RTT),tonumber(LAT),tonumber(DRIFT),tonumber(FREEZE),tonumber(LATE)
-    for i=1, 4 do
-        l = f:read'*l'
-        local lbl,frames,evt,rtt,lat,drift,freeze,late =
-            string.match(l, "([^%s]*)%s([^%s]*)%s([^%s]*)%s([^%s]*)%s([^%s]*)%s([^%s]*)%s([^%s]*)%s([^%s]*)")
-        --print(lbl,frames,evt,rtt,lat,drift,freeze,late)
-        assert(LBL == lbl)
-        local frames = tonumber(frames)
-        local evt    = tonumber(evt)
-        local rtt    = tonumber(rtt)
-        local lat    = tonumber(lat)
-        local drift  = tonumber(drift)
-        local freeze = tonumber(freeze)
-        local late   = tonumber(late)
-        print(l)
-        FRAMES = FRAMES + frames --; assert(1.1>=frames and frames>=0.9)
-        --print(a2,evt)
-        EVT    = EVT    + evt    --; assert(a2*1.1>=evt    and evt   *1.1>=a2)
-        --print(a3,rtt)
-        RTT    = RTT    + rtt    --; assert(a3*1.5>rtt    and rtt   *1.5>=a3)
-        --print(a4,lat)
-        LAT    = LAT    + lat    --; assert(a4*1.2>=lat    and lat   *1.2>=a4)
-        --print(a5,drift)
-        DRIFT  = DRIFT  + drift  --; assert(a5*1.2>=drift  and drift *1.2>=a5)
-        --print(a6,freeze)
-        FREEZE = FREEZE + freeze --; assert(a6*1.2>=freeze and freeze*1.2>=a6)
-        --print(a7,late)
-        LATE   = LATE   + late   --; assert(a7*1.2>=late   and late  *1.2>=a7)
-    end
+
+    frames = tonumber(frames)
+    evt    = tonumber(evt)
+    rtt    = tonumber(rtt)
+    lat    = tonumber(lat)
+    drift  = tonumber(drift)
+    freeze = tonumber(freeze)
+    late   = tonumber(late)
+
+    t[lbl] = t[lbl] or { n=0, lbl=lbl, frames=0, evt=0, rtt=0, lat=0, drift=0, freeze=0, late=0 }
+    local T = t[lbl]
+
+    T.n      = T.n + 1
+    T.frames = T.frames + frames --; assert(1.1>=frames and frames>=0.9)
+    T.evt    = T.evt    + evt    --; assert(a2*1.1>=evt    and evt   *1.1>=a2)
+    T.rtt    = T.rtt    + rtt    --; assert(a3*1.5>rtt    and rtt   *1.5>=a3)
+    T.lat    = T.lat    + lat    --; assert(a4*1.2>=lat    and lat   *1.2>=a4)
+    T.drift  = T.drift  + drift  --; assert(a5*1.2>=drift  and drift *1.2>=a5)
+    T.freeze = T.freeze + freeze --; assert(a6*1.2>=freeze and freeze*1.2>=a6)
+    T.late   = T.late   + late   --; assert(a7*1.2>=late   and late  *1.2>=a7)
+
     l = f:read'*l'
-    print(LBL, FRAMES/5, EVT/5, RTT/5, LAT/5, DRIFT/5, FREEZE/5, LATE/5)
+end
+
+local x = {}
+for k in pairs(t) do
+    --print(k)
+    x[#x+1] = k
+end
+table.sort(x)
+
+for _,k in ipairs(x) do
+    local T = t[k]
+    print(T.lbl, T.frames/T.n, T.evt/T.n, T.rtt/T.n, T.lat/T.n, T.drift/T.n, T.freeze/T.n, T.late/T.n)
 end
