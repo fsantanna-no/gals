fb24ab76c3bc47a1a22cd84fd9692747a175d0ea
Francisco Sant'Anna <francisco.santanna@gmail.com>
2021-07-21 19:59:04 -0300

    ...

--- src/kt/src/test/eval.sh
+++ src/kt/src/test/eval.sh
@@ -4,7 +4,7 @@ for i in 1 2 3
 do
     for N in 2 5 10 50 100
     do
-        for EVT in 10000 5000 1000 500 250 100
+        for EVT in 5000 1000 500 250 100
         do
             for FPS in 10 20 40 50 100
             do
