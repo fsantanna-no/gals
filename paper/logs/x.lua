b0d56d4a5e98960a528a64e1f5b0bb67d8aef51d
Francisco Sant'Anna <francisco.santanna@gmail.com>
2021-07-21 19:55:44 -0300

    Lua logs

--- paper/logs/x.lua
+++ paper/logs/x.lua
@@ -1,3 +1,8 @@
+-- $ grep XXX rem.log > rem.xxx
+-- $ lua5.4 x.lua rem.xxx > rem.x
+-- $ lua5.4 y.lua rem.x > rem.y
+
+
 function dec (n)
     return math.floor(n*100)/100
 end
@@ -8,7 +13,7 @@ print('  N-FPS-RATE', 'FRAMES', 'EVT', 'RTT', 'LAT', 'DRIFT', 'FREEZE', 'LATE')
 while l do
     local dir,n,fps,evt,time,frames,evts,rtt,lat,_,drift,_,freeze,_,late = string.match(l, "XXX ; ([^ ]*) ; 50.110. ; 600 ; (%d+) ; (%d+) ; (%d+) ; (%d+) ; (%d+) ; (%d+) ; %s*([^ ]*) ; %s*([^ ]*) ; (%d+);([^ ]*) ; (%d+);([^ ]*) ; (%d+);([^ ]*)")
     --print(l)
-    print(string.format('%03d',n)..'-'..string.format('%03d',fps)..'-'..string.format('%05d',math.floor(evt/fps)),
+    print(string.format('%03d',n)..'-'..string.format('%03d',fps)..'-'..string.format('%05d',math.floor(evt/n)),
           dec(frames/(time/(1000/fps))),
           dec(evts/(time/evt)),
           rtt, lat, drift, freeze, late)
--- paper/logs/y.lua
+++ paper/logs/y.lua
@@ -5,19 +5,34 @@ l = f:read'*l'
 while l do
     local LBL,FRAMES,EVT,RTT,LAT,DRIFT,FREEZE,LATE =
         string.match(l, "([^%s]*)%s([^%s]*)%s([^%s]*)%s([^%s]*)%s([^%s]*)%s([^%s]*)%s([^%s]*)%s([^%s]*)")
+    local a1,a2,a3,a4,a5,a6,a7 = tonumber(FRAMES),tonumber(EVT),tonumber(RTT),tonumber(LAT),tonumber(DRIFT),tonumber(FREEZE),tonumber(LATE)
     for i=1, 4 do
         l = f:read'*l'
         local lbl,frames,evt,rtt,lat,drift,freeze,late =
             string.match(l, "([^%s]*)%s([^%s]*)%s([^%s]*)%s([^%s]*)%s([^%s]*)%s([^%s]*)%s([^%s]*)%s([^%s]*)")
         --print(lbl,frames,evt,rtt,lat,drift,freeze,late)
         assert(LBL == lbl)
-        FRAMES = FRAMES + frames
-        EVT    = EVT    + evt
-        RTT    = RTT    + rtt
-        LAT    = LAT    + lat
-        DRIFT  = DRIFT  + drift
-        FREEZE = FREEZE + freeze
-        LATE   = LATE   + late
+        local frames = tonumber(frames)
+        local evt    = tonumber(evt)
+        local rtt    = tonumber(rtt)
+        local lat    = tonumber(lat)
+        local drift  = tonumber(drift)
+        local freeze = tonumber(freeze)
+        local late   = tonumber(late)
+        print(l)
+        FRAMES = FRAMES + frames --; assert(1.1>=frames and frames>=0.9)
+        --print(a2,evt)
+        EVT    = EVT    + evt    --; assert(a2*1.1>=evt    and evt   *1.1>=a2)
+        --print(a3,rtt)
+        RTT    = RTT    + rtt    --; assert(a3*1.5>rtt    and rtt   *1.5>=a3)
+        --print(a4,lat)
+        LAT    = LAT    + lat    --; assert(a4*1.2>=lat    and lat   *1.2>=a4)
+        --print(a5,drift)
+        DRIFT  = DRIFT  + drift  --; assert(a5*1.2>=drift  and drift *1.2>=a5)
+        --print(a6,freeze)
+        FREEZE = FREEZE + freeze --; assert(a6*1.2>=freeze and freeze*1.2>=a6)
+        --print(a7,late)
+        LATE   = LATE   + late   --; assert(a7*1.2>=late   and late  *1.2>=a7)
     end
     l = f:read'*l'
     print(LBL, FRAMES/5, EVT/5, RTT/5, LAT/5, DRIFT/5, FREEZE/5, LATE/5)
