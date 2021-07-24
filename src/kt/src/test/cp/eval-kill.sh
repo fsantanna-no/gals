b8462b2aef21e8e3f7704a4d05aa5b9bb80bd564
Francisco Sant'Anna <francisco.santanna@gmail.com>
2021-07-24 11:54:15 -0300

    ...

--- src/kt/src/test/cp/eval-kill.sh
+++ src/kt/src/test/cp/eval-kill.sh
@@ -5,11 +5,13 @@ I5=192.168.0.13
 I7=192.168.0.12
 P2=192.168.0.5
 LC=152.92.236.9
-USER=chico
-PASS=password
+USER1=xxx
+PASS1=xxx
+USER2=xxx
+PASS2=xxx
 
 pkill -f GALS.jar
-sshpass -p $PASS ssh "$USER@$I5" -f "pkill -f GALS.jar"
-sshpass -p $PASS ssh "$USER@$I7" -f "pkill -f GALS.jar"
-sshpass -p $PASS ssh "$USER@$P2" -f "pkill -f GALS.jar"
-sshpass -p $PASS ssh "$USER@$LC" -f "pkill -f GALS.jar"
+sshpass -p $PASS1 ssh "$USER1@$I5" -f "pkill -f GALS.jar"
+sshpass -p $PASS1 ssh "$USER1@$I7" -f "pkill -f GALS.jar"
+sshpass -p $PASS1 ssh "$USER1@$P2" -f "pkill -f GALS.jar"
+sshpass -p $PASS2 ssh "$USER2@$LC" -f "pkill -f GALS.jar"
