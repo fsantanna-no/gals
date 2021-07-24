d417e0838401d7ded5cba359a0ca31452a9c5fd1
Francisco Sant'Anna <francisco.santanna@gmail.com>
2021-07-24 07:52:10 -0300

    ...

--- src/kt/src/test/cp/eval-kill.sh
+++ src/kt/src/test/cp/eval-kill.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-SRV=192.168.0.17
-I5=192.168.0.11
-I7=192.168.0.10
-#DL=192.168.0.28
+SRV=192.168.0.14
+I5=192.168.0.13
+I7=192.168.0.12
+DL=192.168.0.5
 USER=chico
 PASS=password
 
 pkill -f GALS.jar
 sshpass -p $PASS ssh "$USER@$I5" -f "pkill -f GALS.jar"
 sshpass -p $PASS ssh "$USER@$I7" -f "pkill -f GALS.jar"
-#sshpass -p $PASS ssh "$USER@$DL" -f "pkill -f GALS.jar"
+sshpass -p $PASS ssh "$USER@$DL" -f "pkill -f GALS.jar"
